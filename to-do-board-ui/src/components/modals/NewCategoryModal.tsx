37ad24b67ed9674e5cb6bbdbeef6d507e8575e0d
Atiq Ahammed <atiqahammedshamim@gmail.com>
2023-12-06 01:25:07 +0600

    feat: from added for add category

--- to-do-board-ui/src/components/modals/NewCategoryModal.tsx
+++ to-do-board-ui/src/components/modals/NewCategoryModal.tsx
@@ -1,70 +1,100 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+
+type Inputs = {
+    name: string
+}
 
 export default function NewCategoryModal() {
+    const saveCategory = (data: any) => {
+        console.log(data)
+    }
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>()
     const [showModal, setShowModal] = React.useState(false)
+
     return (
         <>
             <button
-                className="bg-green-200 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                className="bg-green-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                 type="button"
                 onClick={() => setShowModal(true)}
             >
-                Open regular modal
+                + Add Category
             </button>
             {showModal ? (
                 <>
-                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                            {/*content*/}
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                {/*header*/}
-                                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                                    <h3 className="text-3xl font-semibold">
-                                        Modal Title
-                                    </h3>
-                                    <button
-                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                            ×
-                                        </span>
-                                    </button>
-                                </div>
-                                {/*body*/}
-                                <div className="relative p-6 flex-auto">
-                                    <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-                                        I always felt like I could do anything.
-                                        That’s the main thing people are
-                                        controlled by! Thoughts- their
-                                        perception of themselves! They're slowed
-                                        down by their perception of themselves.
-                                        If you're taught you can’t do anything,
-                                        you won’t do anything. I was taught I
-                                        could do everything.
-                                    </p>
-                                </div>
-                                {/*footer*/}
-                                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                                    <button
-                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Close
-                                    </button>
-                                    <button
-                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Save Changes
-                                    </button>
+                    <form onSubmit={handleSubmit(saveCategory)}>
+                        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                                {/*content*/}
+                                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                    {/*header*/}
+                                    <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                                        <h3 className="text-1xl font-semibold">
+                                            Add new Category
+                                        </h3>
+                                    </div>
+                                    {/*body*/}
+                                    <div className="relative p-6 flex-auto">
+                                        <div className="mb-6">
+                                            <label
+                                                htmlFor="default-input"
+                                                className="block mb-2 text-sm font-medium"
+                                            >
+                                                Category Name
+                                            </label>
+                                            <input
+                                                {...register('name', {
+                                                    required: true,
+                                                    maxLength: 100,
+                                                })}
+                                                type="text"
+                                                id="default-input"
+                                                className="border w-[600px] border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            />
+                                            {errors.name &&
+                                                errors.name.type ===
+                                                    'required' && (
+                                                    <span className='text-red-500'>
+                                                        This is required
+                                                    </span>
+                                                )}
+                                            {errors.name &&
+                                                errors.name.type ===
+                                                    'maxLength' && (
+                                                    <span className='text-red-500'>
+                                                        Max length exceeded
+                                                    </span>
+                                                )}
+                                        </div>
+                                    </div>
+                                    {/*footer*/}
+
+                                    <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                                        <button
+                                            className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                            type="button"
+                                            onClick={() => setShowModal(false)}
+                                        >
+                                            Close
+                                        </button>
+                                        <button
+                                            className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                            type="submit"
+                                        >
+                                            Save
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>{' '}
+                    </form>
                 </>
             ) : null}
         </>
