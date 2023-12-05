9726d1ae0580347ab9343b069fa89b94dd1fd341
Atiq Ahammed <atiqahammedshamim@gmail.com>
2023-12-05 16:35:45 +0600

    Feat: Nav Added

--- to-do-board-ui/src/components/NoPage.tsx
+++ to-do-board-ui/src/components/NoPage.tsx
@@ -1,6 +1,6 @@
 function NoPage() {
     return (
-        <div className="page-not-found">
+        <div className="center">
             <h1 className="text-3xl font-bold">Page Not Available</h1>
         </div>
     )
--- to-do-board-ui/src/components/ToDoMaster.tsx
+++ to-do-board-ui/src/components/ToDoMaster.tsx
@@ -1,8 +1,14 @@
+import { Fragment } from "react";
+import Nav from "./Nav";
+
 function ToDoMaster() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      To Do Page
-    </h1>
+    <Fragment>
+      <Nav/>
+      <h1 className="text-3xl font-bold underline">
+        To Do Page
+      </h1>
+    </Fragment>
   )
 }
 
