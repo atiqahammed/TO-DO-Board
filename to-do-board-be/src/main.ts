0b036d5eddb029700bf1a5f15817dede49c78661
Atiq Ahammed <atiqahammedshamim@gmail.com>
2023-12-06 14:47:42 +0600

    health API added

--- to-do-board-be/src/main.ts
+++ to-do-board-be/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './controllers/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('To Do Service')
+        .setDescription('The To Do API description')
+        .setVersion('1.0')
+        .addTag('to-do')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     const PORT = process.env.PORT || 4500;
     await app.listen(PORT);
 }
